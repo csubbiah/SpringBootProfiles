d4b6e18bc0e438f55eefba9ed1dd3eaa6c763b6a
Subbiah Chockalingam <subbiah.chockalingam@live.com>
2024-06-07 08:32:40 -0400

    Added @ActiveProfile for multiProfile testing

--- src/test/java/com/example/springbootprofiles/SpringbootprofilesApplicationDevProfileTests.java
+++ src/test/java/com/example/springbootprofiles/SpringbootprofilesApplicationDevProfileTests.java
@@ -3,14 +3,14 @@ package com.example.springbootprofiles;
 import static org.junit.jupiter.api.Assertions.*;
 import com.example.springbootprofiles.spring.ProxyFeatureFlagConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.ActiveProfiles;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpringbootprofilesApplicationTests {
+@ActiveProfiles("dev")
+class SpringbootprofilesApplicationDevProfileTests {
 
 
     @Autowired
@@ -19,14 +19,15 @@ class SpringbootprofilesApplicationTests {
 
     @BeforeAll
     static void beforeAll() {
-
     }
 
     @Test
     public void givenprofiles_whenDevProfileSet_ReadDevProfileValues() {
 
-            assertEquals(proxyFeatureFlagConfiguration.getName(), "DEV");
-        }
+        assertEquals(proxyFeatureFlagConfiguration.getName(), "DEV");
+        assertEquals(proxyFeatureFlagConfiguration.enabled, true);
+        assertEquals(proxyFeatureFlagConfiguration.proxyAddress, "http://dev.discoverfinancial.com");
+    }
 
 
 }
