0da9812faecf39772843b24457e110a7291ddac7
Subbiah Chockalingam <subbiah.chockalingam@live.com>
2024-06-07 13:32:37 -0400

    Added Environment Bean Configuration Injection

--- src/main/java/com/example/springbootprofiles/SpringbootprofilesApplication.java
+++ src/main/java/com/example/springbootprofiles/SpringbootprofilesApplication.java
@@ -1,5 +1,7 @@
 package com.example.springbootprofiles;
 
+import com.example.springbootprofiles.model.JWTConsumerKeys;
+import com.example.springbootprofiles.spring.JWTConsumerKeysConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -17,10 +19,18 @@ public class SpringbootprofilesApplication implements CommandLineRunner {
     @Autowired
     public ProxyFeatureFlagConfiguration proxyFeatureFlagConfiguration;
 
+    @Autowired
+    public JWTConsumerKeys jwtConsumerKeys;
+
     @Override
     public void run(String... args) {
+        //Profile Configuration injected as a Component & ConfigurationProperties
         System.out.print(proxyFeatureFlagConfiguration.name);
         System.out.println(proxyFeatureFlagConfiguration.enabled);
         System.out.println(proxyFeatureFlagConfiguration.proxyAddress);
+        //Environment Config Injected as a Bean -- Could be defined in Container OS (or) local OS
+        System.out.print(jwtConsumerKeys.getApiKey());
+        System.out.print(jwtConsumerKeys.getSecretKey());
+        System.out.print(jwtConsumerKeys.getConsumerCert());
     }
 }
